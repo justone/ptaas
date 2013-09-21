868f866cab818c0e9ad77f5126f7fd4d4e901a63
Nate Jones <nate@endot.org>
2013-09-21 23:47:45 +0000

    first working end-to-end version

--- tidy.pl
+++ tidy.pl
@@ -3,10 +3,29 @@
 use strict;
 use warnings;
 
+use IPC::Run3;
+
 print "Content-type: text/plain\n\n";
 
-my $data;
-sysread( STDIN, $data, $ENV{CONTENT_LENGTH} );
-print "$data\n";
+my $query = $ENV{QUERY_STRING};
+
+my $pt_version;
+if ($query) {
+    $pt_version = ( split( /=/, $query ) )[1];
+    my $pt_script = "/tidy/${pt_version}.sh";
+    if ( !-e $pt_script ) {
+        print "No version $pt_version\n";
+        exit;
+    }
+
+    my $data;
+    sysread( STDIN, $data, $ENV{CONTENT_LENGTH} );
+
+    run3 [$pt_script], \$data;
+}
+else {
+    print "Send the version to use with v=\n";
+    exit;
+}
 
 exit;
